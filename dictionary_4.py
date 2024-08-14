73b0a595b897e065ac9bbcfd3954a43885e0c82f
rooshanriaz <riazrooshan@gmail.com>
2024-08-15 02:50:52 +0500

    4nd dictionary problem

---
+++ dictionary_4.py
@@ -0,0 +1,13 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+for name in favorite_languages.keys():
+    print(name)
+
+for language in favorite_languages.values():
+    print(language)
+

874b13107562c5c5f225639f18086a9453215150
rooshanriaz <riazrooshan@gmail.com>
2024-08-15 03:06:01 +0500

    another dictionary problem

--- dictionary_4.py
+++ dictionary_4.py
@@ -12,7 +12,8 @@ for language in favorite_languages.values():
     print(language)
 """
 
-"""favorite_languages = {'jen': 'python',
+
+favorite_languages = {'jen': 'python',
                       'sarah': 'c',
                       'edward': 'ruby',
                       'phil': 'python',}
@@ -23,7 +24,8 @@ for name in favorite_languages.keys():
 
     if name in friends:
         language = favorite_languages[name].title()
-        print(f"\t{name.title()}, I see you love {language}.")"""
+        print(f"\t{name.title()}, I see you love {language}.")
 
-
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll.")
 

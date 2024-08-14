cd6fd217d7cf049116da2e6b118e2f1cbed8e225
rooshanriaz <riazrooshan@gmail.com>
2024-08-15 02:38:14 +0500

    3nd dictionary problem

---
+++ dictionary_3.py
@@ -0,0 +1,12 @@
+languages = {'jen': 'python',
+             'sarah': 'C',
+             'edward': 'rust',
+             'phil': 'python'}
+
+language_1 = languages['sarah'].title()
+language_2 = languages['edward'].title()
+language_3 = languages['phil'].title()
+
+print(f"Sarah's favorite language is {language_1}")
+print(f"Edward's favorite language is {language_2}")
+print(f"Phil's favorite language is {language_3}")

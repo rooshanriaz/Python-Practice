fd60bea44971567ebc46a10553b2a76a45e18b1f
rooshanriaz <riazrooshan@gmail.com>
2024-08-15 03:23:39 +0500

    nested dictionaries

--- dictionary_4.py
+++ dictionary_4.py
@@ -58,3 +58,21 @@ for alien in aliens[:5]:
 print("...")
 
 print(f"Total number of aliens is:{len(aliens)}")"""
+aliens = []
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'fast'
+        alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'medium'
+        alien['points'] = 15
+
+for alien in aliens[:5]:
+    print(alien)
+print("...")

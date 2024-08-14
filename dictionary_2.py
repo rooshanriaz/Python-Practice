21085d07b4066603bb0f547a2400c4696d8eefd5
rooshanriaz <riazrooshan@gmail.com>
2024-08-15 02:37:02 +0500

    2nd dictionary problem

--- dictionary_2.py
+++ dictionary_2.py
@@ -1,11 +1,11 @@
-favorite_languages = {'jen': 'python',
+languages = {'jen': 'python',
                       'sarah': 'C',
                       'edward': 'rust',
                       'phil': 'python'}
 
-language_1 = favorite_languages['sarah'].title()
-language_2 = favorite_languages['edward'].title()
-language_3 = favorite_languages['phil'].title()
+language_1 = languages['sarah'].title()
+language_2 = languages['edward'].title()
+language_3 = languages['phil'].title()
 
 print(f"Sarah's favorite language is {language_1}")
 print(f"Edward's favorite language is {language_2}")

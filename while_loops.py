a7f1389d42e2d88ca737a26c588d0ff5d2a7da49
rooshanriaz <riazrooshan@gmail.com>
2024-08-16 01:27:28 +0500

    using continue and break in loops

--- while_loops.py
+++ while_loops.py
@@ -3,10 +3,32 @@ while current_number <= 5:
     print(current_number)
     current_number += 1"""
 
-"""prompt = "\n Tell me something, and I will repeat it back to you."
+prompt = "\n Tell me something, and I will repeat it back to you."
 prompt += "\n Enter 'quit' to end the program."
 
-message = ""
+"""message = ""
 while message != "quit":
     message = input(prompt)
     print(message)"""
+
+"""active = True
+while active:
+    message = input(prompt)
+    if message == 'quit':
+        active = False
+    else:
+        print(message)"""
+
+"""while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print(f"I'd love to go to {city}")"""
+"""current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue
+    print(current_number)"""

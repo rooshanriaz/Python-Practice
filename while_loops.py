141052fdac28a55dd595f5ccfb61c421996054cf
rooshanriaz <riazrooshan@gmail.com>
2024-08-16 01:23:43 +0500

    started while loops

---
+++ while_loops.py
@@ -0,0 +1,12 @@
+"""current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1"""
+
+"""prompt = "\n Tell me something, and I will repeat it back to you."
+prompt += "\n Enter 'quit' to end the program."
+
+message = ""
+while message != "quit":
+    message = input(prompt)
+    print(message)"""

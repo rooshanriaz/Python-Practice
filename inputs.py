3f5efd778cfcb5183fdabea624e2b278152eb499
rooshanriaz <riazrooshan@gmail.com>
2024-08-16 01:13:56 +0500

    concatenation in inputs

--- inputs.py
+++ inputs.py
@@ -4,3 +4,8 @@ print(message)"""
 """name = input("Please enter your name:")
 print(name)"""
 
+"""prompt = "If you share your name, we can personalize the message you see."
+prompt += "\n What is your first name ?"
+name = input(prompt)
+
+print(f"\n Hello, {name}")"""

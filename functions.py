8473eccd3a2b750e74d8b603b650e5ced77476e9
rooshanriaz <riazrooshan@gmail.com>
2024-08-17 23:06:40 +0500

    mooc.fi exercise problem

--- functions.py
+++ functions.py
@@ -1,6 +1,17 @@
-def message():
+"""def message():
     print("Hello, My name is Joe.")
 
 message()
 message()
-message()
+message()"""
+def seven_brothers():
+    print('''
+    Aapo
+    Eero
+    Juhani
+    Lauri
+    Simeoni
+    Timo
+    Tuomas''')
+
+seven_brothers()
